9076887d84b2d4bb7fb90107187d9dfedaafa486
Cheeseburrger <hellohaygood@gmail.com>
2025-04-19 18:13:23 -0500

    fadein/fadeout/wave music
    Hellooooo worldd

--- Scripts/Main System/enemy_wave.gd
+++ Scripts/Main System/enemy_wave.gd
@@ -10,6 +10,7 @@ var moveSpeed : int = 3
 var currEnemyTotal : int = 0
 var roundStarted : bool = false
 var blockLocation : Array = []
+var roundCount : int = 0
 
 func newCurve(startPos:Vector2i,rows:int,cols:int) -> Curve3D:
 	#construct curve for path (CHANGE THIS INGAME)
@@ -84,6 +85,65 @@ func newCurve(startPos:Vector2i,rows:int,cols:int) -> Curve3D:
 	
 	return curve
 
+func newPath() -> void:
+	var startPos:Vector2i = Vector2i(0.5,0.5)
+	var rows = TowerSpawner.col - 1 #(x)
+	var cols = TowerSpawner.row - 1 #(z)
+	self.curve = newCurve(startPos,rows,cols)
+
+
+func music(round:int) -> void:
+	#print(AudioServer.get_bus_volume_db(3))
+	if roundStarted:
+		var setVol:float = AudioServer.get_bus_volume_db(3)/1.06
+		if setVol >= -0.05:
+			setVol = 0;
+		if setVol < 0:
+			match(round):
+				4:
+					AudioServer.set_bus_volume_db(6,setVol)
+					AudioServer.set_bus_volume_db(5,setVol)
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				3:
+					AudioServer.set_bus_volume_db(5,setVol)
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				2:
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				1:
+					AudioServer.set_bus_volume_db(3,setVol)
+				0:
+					pass
+				_:
+					print("Error: fadeIn: Uncoded round")
+	else:
+		var setVol:float = AudioServer.get_bus_volume_db(3) * 1.02
+		if setVol == 0:
+			setVol = -.1
+		if setVol >= -100:
+			match(round):
+				4:
+					AudioServer.set_bus_volume_db(6,setVol)
+					AudioServer.set_bus_volume_db(5,setVol)
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				3:
+					AudioServer.set_bus_volume_db(5,setVol)
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				2:
+					AudioServer.set_bus_volume_db(4,setVol)
+					AudioServer.set_bus_volume_db(3,setVol)
+				1:
+					AudioServer.set_bus_volume_db(3,setVol)
+				0:
+					pass
+				_:
+					print("Error: fadeIn: Uncoded round")
+
+
 func _on_timer_timeout() -> void:
 	var newEnemy = ENEMY_PATH.instantiate()
 	mainPath.add_child(newEnemy)
@@ -93,12 +153,11 @@ func _on_ui_start_round() -> void:
 	#print("Creating Curve") #debug
 	#Risk.print2DArray(Risk.calculate_path(Vector2i(0,0),Vector2i(19,14)))
 	#var curve:Curve3D = $"../Path3D".newCurve()
-	var startPos:Vector2i = Vector2i(0.5,0.5)
-	var rows = TowerSpawner.col - 1 #(x)
-	var cols = TowerSpawner.row - 1 #(z)
-	self.curve = newCurve(startPos,rows,cols)
 	if !roundStarted:
-		print("round start")
+		newPath()
+		print("round start: ")
+		roundCount += 1
+		print(roundCount)
 		timer.set_wait_time(EnemySpawner.path1D[EnemySpawner.roundCounter][1])
 		timer.start()
 
@@ -112,6 +171,7 @@ func _process(_delta: float) -> void:
 		if EnemySpawner.roundCounter < EnemySpawner.totalRounds:
 			EnemySpawner.roundCounter += 1
 		roundStarted = false
+	music(roundCount)
 
 func _ready() -> void:
 	var used_cells = main_map.get_used_cells()
