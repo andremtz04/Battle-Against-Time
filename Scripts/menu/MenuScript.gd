1e31d33069adb8e8b93872753eb4da9c287308ed
andremtz04 <159098727+andremtz04@users.noreply.github.com>
2025-04-22 20:03:12 -0500

    added basic main menu

--- Scripts/menu/MenuScript.gd
+++ Scripts/menu/MenuScript.gd
@@ -1,5 +1,6 @@
-extends Control
+extends Node3D
 
+@onready var camera: Camera3D = $Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,9 +9,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	camera.rotate_y(0.3 * delta)
 
 
 func _on_start_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 	# Replace with function body.
+
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
